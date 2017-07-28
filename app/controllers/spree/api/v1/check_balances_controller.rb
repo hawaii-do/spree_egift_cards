8d0bc5c1add79b7ce5718bdfe5ea9f6dd0f669df
renoke <renaud.kern@gmail.com>
2017-07-28 13:26:52 +0200

    Add debug to check_balance

--- app/controllers/spree/api/v1/check_balances_controller.rb
+++ app/controllers/spree/api/v1/check_balances_controller.rb
@@ -1,13 +1,19 @@
 class Spree::Api::V1::CheckBalancesController < Spree::Api::BaseController
 
+	include Spree::StoresHelper
+
 	def create
+		logger.debug "CHECK BALANCE"
+		logger.debug params.inspect
 		code	= params[:code]
 		pin 	= params[:pin]
 
 		if @egift_card = Spree::EgiftCard.where(code: code, pin: pin).first
+			logger.debug "EgiftCard found"
 			render json: @egift_card
 		else
-			invalid_ressource!
+			logger.debug "EgiftCard not found"
+			render "spree/api/v1/egift_cards/not_found", status: 422
 		end
 	end
 
